8e4de2b3482f83728738783cdb5558988a6cde7a
ZVildan <zvildanaltuntas@gmail.com>
2025-02-23 11:17:06 +0100

    v4

--- src/deneme_github/GitHubDeneme.java
+++ src/deneme_github/GitHubDeneme.java
@@ -3,13 +3,15 @@ package deneme_github;
 public class GitHubDeneme {
     // GitHub kullanmayi ogreniyorum
 
-<<<<<<< HEAD
+
 
 
 
 
     // ekledim
-=======
+
     // Sanirim ogrenmeye basladim
->>>>>>> 54bd2c465cb652344f2f23ebeaa01feb25b5008d
+
+    // Hadi insaAllah
+
 }
